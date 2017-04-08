b6b7233ee9ce311ff7a5808aedddc9569688452e
joelakwes <joelakwes@gmail.com>
2017-04-08 12:20:38 +0100

    Edited function to pass newly added test case that checks if all array elements are numbers

--- aritgeo.js
+++ aritgeo.js
@@ -2,39 +2,49 @@
 
 module.exports = {
 	aritGeo: function(numberArray){
+		// Function to Check if all elements of array are numbers
+		function allNumbers(element, index, array) { 
+			return (typeof element == "number") ? true : false;
+		} 
+
 		// check if numberArray is empty and return 0
 		if (numberArray == undefined || numberArray.length == 0) {
 			return 0;
 		}
 
-		var commonRatio = numberArray[1]/numberArray[0];
-		var commonDifference = numberArray[1] - numberArray[0];
-		var arithmetic = true;
-		var geometric = true;
+		// Check if all elements of array are numbers
+		if(numberArray.every(allNumbers)){
+			var commonRatio = numberArray[1]/numberArray[0];
+			var commonDifference = numberArray[1] - numberArray[0];
+			var arithmetic = true;
+			var geometric = true;
 
-		if(numberArray.length >= 3){
-			for(var i = 0; i < numberArray.length; i++){
-				// Using the formula nthTerm = firstTerm + (n - 1)d
-				// since js is zero indexed, i = n-1.  
-				if(numberArray[i] != numberArray[0] + (i * commonDifference)){
-					arithmetic = false;
+			if(numberArray.length >= 3){
+				for(var i = 0; i < numberArray.length; i++){
+					// Using the formula nthTerm = firstTerm + (n - 1)d
+					// since js is zero indexed, i = n-1.  
+					if(numberArray[i] != numberArray[0] + (i * commonDifference)){
+						arithmetic = false;
+					}
+					// Using the formula of nth term of GP Xn = ar**(n - 1)
+					if(numberArray[i] != numberArray[0] * (Math.pow(commonRatio, i)) ){
+						geometric = false;
+					}
 				}
-				// Using the formula of nth term of GP Xn = ar**(n - 1)
-				if(numberArray[i] != numberArray[0] * (Math.pow(commonRatio, i)) ){
-					geometric = false;
-				}
-			}
 
-			if(arithmetic == true){
-				return 'Arithmetic';
-			}
-			else if(geometric == true){
-				return 'Geometric';
+				if(arithmetic == true){
+					return 'Arithmetic';
+				}
+				else if(geometric == true){
+					return 'Geometric';
+				} else {
+					return -1;
+				}
 			} else {
-				return -1;
+				return "Array should contain at least 3 numbers";
 			}
 		} else {
-			return "Array should contain at least 3 numbers";
+			return "All elements in Array should be numbers";
 		}
 	}	
 }
