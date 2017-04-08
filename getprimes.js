aa10ce46b93e9209b2c17c7ffa1739558983e001
joelakwes <joelakwes@gmail.com>
2017-04-08 01:46:38 +0100

    Declared dependencies, write unit tests next

---
+++ getprimes.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.export = {
+	getPrimes: function(number){
+		
+	}
+}
---
+++ tests/test.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+
+  var jasmine = require('jasmine');
+  var getPrimes = require('../getprimes.js').getPrimes;
+
+})();
